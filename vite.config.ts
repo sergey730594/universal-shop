9a0cf5b5d73e14aec0be26773e6ba728b48ccf25
sergey730594 <sergey730594@gmail.com>
2026-08-22 07:41:44 +0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// 👇 Добавь эту строку
+const baseName = '/shop-tamplate/' // или '/shop-template/' если переименуешь
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // 👇 Добавь эту строку
+  base: baseName,
+  server: { 
+    port: 5173 
+  }
 })
