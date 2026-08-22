181a2cd1003e42c7c66e00cd5cd1c828f6a1902b
null <null>
2026-08-22 14:43:52 +0400

    fix name

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // 👇 Добавь эту строку
-const baseName = '/vite-react-7y74tlxv/' //
+const baseName = '/universal-shop/' //
 
 export default defineConfig({
   plugins: [react()],
