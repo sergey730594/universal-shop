e639988b6cb535e0e27e6eb19ffa97e59a6e6b27
sergey730594 <sergey730594@gmail.com>
2026-08-22 08:05:35 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // 👇 Добавь эту строку
-const baseName = '/vite-react-7y74tlxv/' // или '/vite-react-7y74tlxv/' если переименуешь
+const baseName = '/vite-react-7y74tlxv/' //
 
 export default defineConfig({
   plugins: [react()],
