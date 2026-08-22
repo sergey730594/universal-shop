1b3bd8b2ccbd817f4dbca405ebcecc14fb8b82b2
sergey730594 <sergey730594@gmail.com>
2026-08-22 07:52:08 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // 👇 Добавь эту строку
-const baseName = '/shop-tamplate/' // или '/shop-template/' если переименуешь
+const baseName = '/vite-react-7y74tlxv/' // или '/vite-react-7y74tlxv/' если переименуешь
 
 export default defineConfig({
   plugins: [react()],
